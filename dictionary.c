8150087442f9dfb6174f93d3db83926b2e672e65
Wachuley <132625407+Wachuley@users.noreply.github.com>
2024-09-19 10:40:21 -0600

    Update dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -1,7 +1,13 @@
 #include "dictionary.h"
 
 void initializeDataDictionary(const char *dictionaryName) {
+    long mainHeader = -1;
+    
     printf("Initializing Data Dictionary...\n");
 
     FILE *dictionary = fopen(DictionaryName, "w+");
+
+    fwrite(&mainHeader, sizeof(mainHeader), 1, dictionary);
+
+    long currentDir = ftell(dictionary);
 }
