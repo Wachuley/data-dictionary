eb75230fa4c20356df7c0bf7b0356075b0c651fb
Wachuley <132625407+Wachuley@users.noreply.github.com>
2024-09-20 10:24:36 -0600

    Update dictionary.h

--- dictionary.h
+++ dictionary.h
@@ -4,6 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Node
+{
+  char value[50];
+  //struct Node *sig; <- Apuntador en memorie
+  long next;
+} NODE;
+
 #define EMPTY_POINTER -1
 
 int initializeDataDictionary(const char *dictionaryName);
